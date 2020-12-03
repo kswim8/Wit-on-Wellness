a4a230aeac81dd363975aff921236c5ea920c405
kswim8 <kenhuang100@gmail.com>
2020-12-03 02:29:45 -0500

    Update simplexalgo.py

--- test items/simplexalgo.py
+++ test items/simplexalgo.py
@@ -1,47 +1,50 @@
-def simplexAlgorithm(totalCost):
-    chickfila_chicken_biscuit = [450, -21,  -17,    -2.19]
-    hash_browns =               [240, -16,  -2,     -0.99]
-    icedream_cone =             [170, -4,   -5,     -1.25]
-    totalCalories =             0
-    totalFat =                  50
-    totalProtein =              30
-    Z =                         0
-    d = e = f =                 None
-    solution =                  [0, 0, 0, totalCost, 50, 30]
-    #                                                   a                      b                c
-    slackForm = [[Z,       totalCalories, chickfila_chicken_biscuit[0], hash_browns[0], icedream_cone[0], 0, 0, 0],
-                 [d,       totalCost,     chickfila_chicken_biscuit[3], hash_browns[3], icedream_cone[3], 0, 0, 0],
-                 [e,       totalFat,      chickfila_chicken_biscuit[1], hash_browns[1], icedream_cone[1], 0, 0, 0],
-                 [f,       totalProtein,  chickfila_chicken_biscuit[2], hash_browns[2], icedream_cone[2], 0, 0, 0]  ]
+# Keren Huang
 
-    while True:
-        if chickfila_chicken_biscuit[0] >= 0 and hash_browns[0] >= 0 and icedream_cone[0] >= 0:
-            # finding tightest constraint
-            listOfConstraints = []
-            listOfConstraints.append(-1 * totalCost / chickfila_chicken_biscuit[3]) 
-            listOfConstraints.append(-1 * totalFat / chickfila_chicken_biscuit[1])
-            listOfConstraints.append(-1 * totalProtein / chickfila_chicken_biscuit[2])
-            tighestConstraint = min(listOfConstraints)
-            print(tighestConstraint)
+# manually coding simplex algorithm for optimization of linear systems
 
-            # rearrange the equation where the tighest constraint was found
-            tighestConstraintEquation = listOfConstraints.index(tighestConstraint)
-            # since the tighest constraint here was protein, 30/17 = 1.7647, we rearrange f and chickfila_chicken_biscuit[2]
-            newEquation = [1, -1 * totalProtein / chickfila_chicken_biscuit[2], hash_browns[2] / chickfila_chicken_biscuit[2], icedream_cone[2] / chickfila_chicken_biscuit[2], f / chickfila_chicken_biscuit[2]]
+def simplexAlgorithm(totalCost, totalFat, totalProtein):
+    fooditem1name = 'Chick-fil-A® Chicken Biscuit'
+    fooditem2name = 'Hash Browns'
+    fooditem3name = 'Icedream® Cone'
+    # food item        calories, fat, protein, cost
+    fooditem1 =        [450,    -21,  -17,    -2.19]
+    fooditem2 =        [240,    -16,  -2,     -0.99]
+    fooditem3 =        [170,    -4,   -5,     -1.25]
+    totalCalories =    0
+    solution =         [0, 0, 0, totalCost, totalFat, totalProtein]
 
-            
+    a0, a1, a2, a3 = fooditem1[0], fooditem1[1], fooditem1[2], fooditem1[3]
+    b0, b1, b2, b3 = fooditem2[0], fooditem2[1], fooditem2[2], fooditem2[3]
+    c0, c1, c2, c3 = fooditem3[0], fooditem3[1], fooditem3[2], fooditem3[3]
 
-        else:
-            break
+    #             constant       a   b   c   d  e  f
+    slackForm = [[totalCalories, a0, b0, c0, 0, 0, 0], # calories equation
+                 [totalFat,      a1, b1, c1, 1, 0, 0], # fat equation
+                 [totalProtein,  a2, b2, c2, 0, 1, 0], # protein equation
+                 [totalCost,     a3, b3, c3, 0, 0, 1]] # cost equation
 
-def findTighestConstraint(slackForm):
-    listOfConstraints = []
-    listOfConstraints.append(totalCost / chickfila_chicken_biscuit[3]) 
-    listOfConstraints.append(totalFat / chickfila_chicken_biscuit[1])
-    listOfConstraints.append(totalProtein / chickfila_chicken_biscuit[2])
-    tighestConstraint = min(listOfConstraints)
-    print(tighestConstraint)
+    if fooditem1[0] >= 0:
+        # finding tightest constraint
+        listOfConstraints = []
+        listOfConstraints.append(-1 * totalFat / fooditem1[1])
+        listOfConstraints.append(-1 * totalProtein / fooditem1[2])
+        listOfConstraints.append(-1 * totalCost / fooditem1[3]) 
+        tighestConstraint = min(listOfConstraints)
+        print(listOfConstraints) # fat, protein, cost
+        print(tighestConstraint)
+
+        # rearrange the equation where the tighest constraint was found
+        tighestConstraintEquation = listOfConstraints.index(tighestConstraint)
+        print(tighestConstraintEquation)
+        # since the tighest constraint here was protein, 30/17 = 1.7647, we rearrange f and fooditem1[2]
+        newEquation = [1, -1 * totalProtein / fooditem1[2], fooditem2[2] / fooditem1[2], fooditem3[2] / fooditem1[2], 1 / fooditem1[2]]
+        print(slackForm)
+
+    if fooditem2[0] >= 0:
+        pass
+
+    if fooditem3[0] >= 0:
+        pass
 
 
-
-simplexAlgorithm(20)
+simplexAlgorithm(20, 50, 30)
