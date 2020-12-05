be25811969f95f071cacea67a6fd2142e7a61438
kswim8 <kenhuang100@gmail.com>
2020-12-05 01:00:08 -0500

    Updated credits page

--- creditspage.py
+++ creditspage.py
@@ -9,13 +9,17 @@ class Credits(Mode):
         mode.kerenPic2 = mode.scaleImage(mode.kerenPic, 1)
         mode.alexPic = mode.loadImage(alexpic)
         mode.alexPic2 = mode.scaleImage(mode.alexPic, 1)
+        url = 'https://i.pinimg.com/originals/fe/f7/2f/fef72f73f4f961b4ed6f8e4bb093eb1b.jpg'
+        mode.appIcon = mode.loadImage(url)
+        mode.appIcon2 = mode.scaleImage(mode.appIcon, 4.5/10)
 
     def keyPressed(mode, event):
         if (event.key == 'Escape'):
             mode.app.setActiveMode(mode.app.splashScreenMode)
 
     def redrawAll(mode, canvas):
+        canvas.create_image(mode.width/2, mode.height/2, image=ImageTk.PhotoImage(mode.appIcon2))
         canvas.create_text(mode.width/2, 50, text='Credits', font='Calibri 20 bold')
-        canvas.create_text(mode.width/2, mode.height/2, text='Keren Huang\nMentor: Alex Xie')
+        canvas.create_text(mode.width/2, mode.height - 100, text='Keren Huang | Mentor: Alex Xie', font='Calibri 15 bold')
         canvas.create_image(mode.width/4, mode.height/2, image=ImageTk.PhotoImage(mode.kerenPic2))
         canvas.create_image(3*mode.width/4, mode.height/2, image=ImageTk.PhotoImage(mode.alexPic2))
